d5d72c54fbc3dca760efec30ab4ffd3fbe7d4629
Lucas Muniz <lucaslima1048@gmail.com>
2025-01-22 07:48:23 -0300

    create customer class

---
+++ src/main/java/com/lucas/loans_challenge/domain/Customer.java
@@ -0,0 +1,77 @@
+package com.lucas.loans_challenge.domain;
+
+import java.util.Objects;
+
+public class Customer {
+
+    private String name;
+    private Integer age;
+    private String cpf;
+    private Double income;
+    private String location;
+
+    public boolean isIncomeGreaterOrEqualThan(Double value){
+        return this.income >= value;
+    }
+
+    public boolean isIncomeLowerOrEqualThan(Double value){
+        return this.income <= value;
+    }
+
+    public boolean isIncomeBetween(Double value1, Double value2){
+        return this.income >= value1 && this.income <= value2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(name, customer.name) && Objects.equals(age, customer.age) && Objects.equals(cpf, customer.cpf) && Objects.equals(income, customer.income) && Objects.equals(location, customer.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, cpf, income, location);
+    }
+}
